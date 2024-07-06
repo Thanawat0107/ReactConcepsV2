2d1394f9cc3d88baee0739e48d3781d8aeb2cbc4
Thanawat0107 <qwwasszxx@gmail.com>
2024-07-06 14:51:59 +0700

    app01-การส่ง props จากแม่ไปหาลูก

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { useState } from 'react'
 import Header from './component/Header'
 import StudenList from './component/StudentList'
-
+//
 export default function App() {
-  //เรียกใช้ component Header, StudenList 
+  //เรียกใช้ component Header, StudenList
+  //ส่ง props ไปทำงานที่ component Header
   return (
     <div className='container'>
-      <Header/>
+      <Header title="Home" major="Comcience"/>
       <StudenList/>
     </div>
   )
--- app01-state/src/component/Header.jsx
+++ app01-state/src/component/Header.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Logo from "../Images/logo-shopping.png";
 import "./Header.css";
 
-export default function Header() {
+//รับ props มาจาก component App
+//กระจายค่าออกมา title, major
+export default function Header(pros) {
+  const {title, major} = pros;
   return (
     <nav>
       <img src={Logo} alt="logo" className="logo" />
-      <a href="/">Home</a>
+      <a href="/">{title} {major}</a>
     </nav>
   );
 }
