5ae5f1c8f58322747778f52b23c49cfde56887c0
Thanawat0107 <qwwasszxx@gmail.com>
2024-07-06 07:19:25 +0700

    app01-useState

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  //props
+  const [number, setNumder] = useState(1);
+  //สร้างเป็น ฟังก์ชันเรียกใช้งาน
+  const Add = () => {
+    setNumder(number + 1);
+  };
+  const DeletE = () => setNumder(number - 1);
+  const Resset = () => setNumder(0);
+  //เรียกใช้ฟังก์ชันจาก const Add,DeletE,Reset
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>{number}</p>
+      <button className="btn" onClick={Add}>
+        Add
+      </button>
+      <button className="btn" onClick={DeletE}>
+        Delete
+      </button>
+      <button className="btn" onClick={Resset}>
+        Resset
+      </button>
+    </div>
+  );
 }
-
-export default App
