78f4e260c9acb6f9d818e4ba5c86b9dd53314e90
Thanawat0107 <qwwasszxx@gmail.com>
2024-07-13 11:18:56 +0700

    app04-even_onClick

--- app04-hooks/src/components/ProductList.jsx
+++ app04-hooks/src/components/ProductList.jsx
@@ -4,28 +4,40 @@ import "./ProductList.css";
 
 export default function ProductList() {
   const [Products, setProducts] = useState();
+  const [Count, setCount] = useState(0);
+  const [Url, setUrl] = useState("http://localhost:3000");
 
   useEffect(() => {
-    fetch("http://localhost:3000/products")
+    fetch(Url)
       .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        setProducts(data);
+        setCount(data.length);
+      })
       .catch((e) => console.log("Not found"));
-  }, []);
+  }, [Url]);
   return (
-    <div className="product-list">
-      {Products &&
-        Products.map((products) => (
-          <div className="product-card" key={products.id}>
-            <div className="product-id">{products.id}</div>
-            <div className="product-details">
-              <h2>{products.name}</h2>
-              <p>${products.price}</p>
-              <span className="product-status">
-                {products.in_stock ? "In Stock" : "Unvaliable"}k
-              </span>
+    <>
+      <div className="product-list">
+        <div>
+          <h2>{Count}</h2>
+          <button className="btn" onClick={()=>setUrl("http://localhost:3000/products")}>Load All</button>
+          <button className="btn" onClick={()=>setUrl("http://localhost:3000/products?in_stock=true")}>Load In Stock</button>
+        </div>
+        {Products &&
+          Products.map((products) => (
+            <div className="product-card" key={products.id}>
+              <div className="product-id">{products.id}</div>
+              <div className="product-details">
+                <h2>{products.name}</h2>
+                <p>${products.price}</p>
+                <span className="product-status">
+                  {products.in_stock == "true" ? "In Stock" : "Unvaliable"}
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 }
