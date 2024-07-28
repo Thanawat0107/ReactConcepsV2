9a8b2df21587f58640c9312c6496bbc7c32a28b5
Thanawat0107 <qwwasszxx@gmail.com>
2024-07-28 17:03:15 +0700

    TransformResponse from Query and Parameters  การแปลงค่าหลังจากได้ข้อมูลมาแล้ว

--- app07-redux-query/src/api/destinationApi.jsx
+++ app07-redux-query/src/api/destinationApi.jsx
@@ -8,9 +8,15 @@ export const destinationAPI = createApi({
     //QUERY -> GET
     //MUTATION -> POST/PUT/DELETE
     getAllDestination: builder.query({
-      query: () => "destination",
+      query: () => ({
+        url: "destination",
+        method: "GET",
+        params: {},
+      }),
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
       providesTags: ["Destinations"],
     }),
+
     addDestination: builder.mutation({
       query: (destination) => ({
         url: "destination",
